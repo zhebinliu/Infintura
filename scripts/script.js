beb481a4a39fe6c642c41dfe1e194b59ff8b8433
Zhebin Liu <85538531+zhebinliu@users.noreply.github.com>
2023-08-20 21:58:59 +1000

    update site

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
     const jsonContainer = document.getElementById('jsonContainer');
+    const jsonContent = document.getElementById('jsonContent');
 
     toggleButton.addEventListener('click', function() {
-        if (jsonContainer.classList.contains('hidden')) {
-            jsonContainer.classList.remove('hidden');
-            toggleButton.textContent = 'Hide JSON';
-        } else {
-            jsonContainer.classList.add('hidden');
-            toggleButton.textContent = 'Toggle JSON';
+        jsonContainer.classList.toggle('hidden');
+        if (!jsonContainer.classList.contains('hidden')) {
+            fetchJSON();
         }
     });
 
-    fetch('sfdx-project.json')
-        .then(response => response.json())
-        .then(data => {
-            const formattedJSON = JSON.stringify(data, null, 2);
-            jsonContainer.textContent = formattedJSON;
-        })
-        .catch(error => {
-            console.error('Error fetching JSON:', error);
-            jsonContainer.textContent = 'Error loading JSON.';
-        });
+    function fetchJSON() {
+        fetch('sfdx-project.json')
+            .then(response => response.json())
+            .then(data => {
+                const formattedJSON = JSON.stringify(data, null, 2);
+                jsonContent.textContent = formattedJSON;
+            })
+            .catch(error => {
+                console.error('Error fetching JSON:', error);
+                jsonContent.textContent = 'Error loading JSON.';
+            });
+    }
 });
